6d461ce63fbd65e6589e8d4074f36821e5f16e00
savacan <sabacan.hige@gmail.com>
2020-11-05 16:44:09 +0900

    fix: deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Hoge } from "./interface/test";
+
+const hoge: Hoge = {
+  title: "Hello Happy World!"
+};
 
 const App: React.FC = () => {
-  return <div>hello world.</div>;
+  return <div>{hoge}</div>;
 };
 
 const el: HTMLElement | null = document.getElementById("app");
